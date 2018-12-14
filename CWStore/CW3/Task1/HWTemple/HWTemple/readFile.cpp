9dbc249f66ab6e425c14580b0461267b81c40356
Agesyne <A40gE20Low8@mail.ru>
2018-12-14 17:34:28 +0300

    CW3

---
+++ CWStore/CW3/Task1/HWTemple/HWTemple/readFile.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "readFile.h"
+using namespace std;
+
+int readFile(string text, vector<string> &data)
+{
+	ifstream inputDataFile(text, ios::in);
+	if (!inputDataFile.is_open())
+	{
+		cout << "File not found!" << endl;
+		return 1;
+	}
+	while (!inputDataFile.eof()) {
+		string buffer;
+		inputDataFile >> buffer;
+		if (buffer == "")
+		{
+			continue;
+		}
+		data.push_back(buffer);
+	}
+	inputDataFile.close();
+}
