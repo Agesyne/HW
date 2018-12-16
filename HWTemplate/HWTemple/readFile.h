cfef35ba7d6aa0f2168bf5f6bdde5c787170a614
Agesyne <A40gE20Low8@mail.ru>
2018-12-16 22:59:00 +0300

    Fixes

--- HWTemplate/HWTemple/readFile.h
+++ HWTemplate/HWTemple/readFile.h
@@ -2,6 +2,5 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
 
-int readFile(string text, vector<string> &data);
+int readFile(std::string text, std::vector<std::string> &data);
