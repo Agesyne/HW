df9229c7f639f41ed85bae14b666bdaaf2e387b3
Agesyne <A40gE20Low8@mail.ru>
2018-12-23 16:39:46 +0300

    Some function added

--- HWTemplate/HWTemple/readFile.h
+++ HWTemplate/HWTemple/readFile.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <iostream>
 #include <string>
 #include <vector>
 
 void readFile(std::string text, std::vector<std::string> &data);
+
+void readFileByLines(std::string text, std::vector<std::string> &data);
